d4c9b3b3fa82809424432287eea149f09508d739
Carol <cgordillo.db@gmail.com>
2023-04-21 16:45:10 +0100

    - added trait

---
+++ app/Traits/HasMentions.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Traits;
+
+    trait HasMentions
+    {
+        public function getMentions(): array
+        {
+            $mentions = [];
+            if( preg_match_all('/data-id=\"([0-9]+)\"/', $this->body, $output_array) ){
+                if( isset($output_array[1]) )
+                    $mentions = $output_array[1];
+            }
+
+            return $mentions;
+        }
+    }
